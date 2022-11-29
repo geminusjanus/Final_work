f35d45c179f20b3693f6a63ed69287d08d158ccd
Nikita <silvernikchuprov@gmail.com>
2022-11-29 11:53:31 +0300

    starting work

--- Final work/Program.cs
+++ Final work/Program.cs
@@ -1,1 +1,8 @@
-﻿
+﻿Console.WriteLine("Введите первые слово, цифру или символ");
+string name1 = Console.ReadLine();
+Console.WriteLine("Введите вторые слово, цифру или символ");
+string name2 = Console.ReadLine();
+Console.WriteLine("Введите третьи слово, цифру или символ");
+string name3 = Console.ReadLine();
+
+
