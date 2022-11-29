b6a28afae89c52ee49ea0e6e050f0442f740e4d4
Nikita <silvernikchuprov@gmail.com>
2022-11-29 17:26:06 +0300

    first function

--- Final work/Program.cs
+++ Final work/Program.cs
@@ -3,3 +3,15 @@ Console.WriteLine("Введите массив строк через пробе�
 string stroke = Console.ReadLine();
 arrayOne = stroke.Split(' ');
 string[] arrayTwo = new string[arrayOne.Length];
+void ArrayTwo(string[] array1, string[] array2)
+{
+    int count = 0;
+    for (int i = 0; i < array1.Length; i++)
+    {
+    if(array1[i].Length <= 3)
+        {
+        array2[count] = array1[i];
+        count++;
+        }
+    }
+}
