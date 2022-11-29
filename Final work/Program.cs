06811e8d063ad960fe587525dac06be796a03943
Nikita <silvernikchuprov@gmail.com>
2022-11-29 17:27:16 +0300

    done

--- Final work/Program.cs
+++ Final work/Program.cs
@@ -15,3 +15,13 @@ void ArrayTwo(string[] array1, string[] array2)
         }
     }
 }
+void PrintArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+ArrayTwo(arrayOne, arrayTwo);
+PrintArray(arrayTwo);
